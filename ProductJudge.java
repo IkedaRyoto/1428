0e2fa905a23c66289ed4f05c7eaaad857ef06567
usuiyuki1 <108254135+usuiyuki1@users.noreply.github.com>
2022-06-27 15:02:04 +0900

    Add files via upload

--- ProductJudge.java
+++ ProductJudge.java
@@ -1,11 +1,12 @@
 class ProductJudgeclass{
 	
-	// 入力	
+	
 	public boolean []  productjudgeMethod(int STOCK[]){
 		
-		// 数字の判定
+		// boolen配列を宣言
 		boolean judge_product [] = new boolean[STOCK.length];
 		
+		// 各商品の在庫が0ならtrueを格納
 		for(int i = 0; i< STOCK.length; i++) {
 			if(STOCK[i] == 0) {
 				judge_product[i] = true;
